03adeb8fa1797ccf1fee5d9138c56c3f207647a9
Leonardo Catello <leonardo.catello23@gmail.com>
2025-01-10 15:40:56 +0100

    testing 3.5 mini

--- phi-test.py
+++ phi-test.py
@@ -2,25 +2,19 @@ from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
 import os
 from dotenv import load_dotenv
 import tensorflow as tf
-from tensorflow.keras import mixed_precision
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 gpus = tf.config.list_physical_devices('GPU')
 
-if gpus:
-    try:
-        tf.config.set_visible_devices(gpus[0], 'GPU')
-    except RuntimeError as e:
-        print(e)
-        
-# Impostare la politica globale su mixed_float16
-policy = mixed_precision.Policy('mixed_float16')
-mixed_precision.set_global_policy(policy)
+if len(gpus) > 0:
+    tf.config.experimental.set_memory_growth(gpus[0], True)
 
 # Carica le variabili dal file .env
 load_dotenv("key.env")
 
 # Path del modello
-MODEL_PATH = os.getenv("MODEL_PATH")
+MODEL_PATH = os.getenv("MODEL_PATH_PHI3")
 
 # Carica il modello e il tokenizer
 model = AutoModelForCausalLM.from_pretrained(MODEL_PATH)
@@ -31,12 +25,13 @@ generator = pipeline(
     "text-generation",
     model=model,
     tokenizer=tokenizer,
-    device_map="auto"
+    framework="tf",
+    device=gpus[0]
 )
 
 # Funzione di interazione
-def chat_with_phi4():
-    print("Benvenuto nel chat CLI con Phi-4! (Digita 'exit' per uscire)")
+def chat_with_phi3():
+    print("Benvenuto nel chat CLI con Phi-3.5! (Digita 'exit' per uscire)")
     system_message = {
         "role": "system",
         "content": "You are a medieval knight and must provide explanations to modern people."
@@ -59,4 +54,4 @@ def chat_with_phi4():
         print(f"AI: {response}")
 
 if __name__ == "__main__":
-    chat_with_phi4()
+    chat_with_phi3()
