cc9fe287486a3d1b6a925e8d7452ec2d6b0bc226
Leonardo Catello <leonardo.catello23@gmail.com>
2025-01-22 15:55:25 +0100

    test with neo4j for graphrag and requirements

---
+++ Neo4j-test.py
@@ -0,0 +1,80 @@
+import dotenv
+import os
+from langchain_neo4j import Neo4jGraph  
+import matplotlib.pyplot as plt
+import networkx as nx
+
+dotenv.load_dotenv("key.env", override=True)
+
+graph = Neo4jGraph(
+    url=os.environ["NEO4J_URI"].replace("http", "bolt"),  # Corregge lo schema URI
+    username=os.environ["NEO4J_USERNAME"],
+    password=os.environ["NEO4J_PASSWORD"],
+)
+
+q_load_articles = """
+LOAD CSV WITH HEADERS
+FROM 'https://raw.githubusercontent.com/dcarpintero/generative-ai-101/main/dataset/synthetic_articles.csv' 
+AS row 
+FIELDTERMINATOR ';'
+MERGE (a:Article {title:row.Title})
+SET a.abstract = row.Abstract,
+    a.publication_date = date(row.Publication_Date)
+FOREACH (researcher in split(row.Authors, ',') | 
+    MERGE (p:Researcher {name:trim(researcher)})
+    MERGE (p)-[:PUBLISHED]->(a))
+FOREACH (topic in [row.Topic] | 
+    MERGE (t:Topic {name:trim(topic)})
+    MERGE (a)-[:IN_TOPIC]->(t))
+"""
+
+graph.query(q_load_articles)
+graph.refresh_schema()
+print(graph.get_schema)
+
+
+# VECTOR INDEX
+from langchain_community.vectorstores import Neo4jVector
+from langchain_ollama import OllamaEmbeddings
+
+embedding_model = OllamaEmbeddings(model="phi3.5:latest")
+
+vector_index = Neo4jVector.from_existing_graph(
+    embedding_model,
+    url=os.environ["NEO4J_URI"],
+    username=os.environ["NEO4J_USERNAME"],
+    password=os.environ["NEO4J_PASSWORD"],
+    index_name="articles",
+    node_label="Article",
+    text_node_properties=["topic", "title", "abstract"],
+    embedding_node_property="embedding",
+)
+
+#Q&A ON SIMILARITY
+from langchain.chains import RetrievalQA
+from langchain_ollama import ChatOllama
+
+llm_model = ChatOllama(model="phi3.5:latest")
+
+vector_qa = RetrievalQA.from_chain_type(llm=llm_model, chain_type="stuff", retriever=vector_index.as_retriever())
+
+r = vector_qa.invoke(
+    {
+        "query": "which articles discuss how AI might affect our daily life? include the article titles and abstracts."
+    }
+)
+print(r["result"])
+
+# Graph-Cypher-Chain w/ LangChain
+from langchain.chains import GraphCypherQAChain
+
+graph.refresh_schema()
+
+llm_model = ChatOllama(temperature=0, model="phi3.5:latest")
+
+cypher_chain = GraphCypherQAChain.from_llm(
+    cypher_llm=llm_model,
+    qa_llm=llm_model,
+    graph=graph,
+    verbose=True,
+)
