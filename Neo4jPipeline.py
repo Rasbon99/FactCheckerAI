4a67334ecc61934ee1b43f30a95ef845d2a71b37
Leonardo Catello <leonardo.catello23@gmail.com>
2025-01-25 13:30:36 +0100

    update pipeline

--- Neo4jPipeline.py
+++ Neo4jPipeline.py
@@ -4,11 +4,17 @@ from langchain_neo4j import Neo4jGraph
 from langchain_community.vectorstores import Neo4jVector
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain.chains import RetrievalQA
+import time
+from log import Logger
 
 class Neo4jPipeline:
-    def __init__(self, env_file="key.env", model_name="phi3.5:latest", index_name="articles"):
+    def __init__(self, env_file="key.env", index_name="articles"):
         # Load environment variables
         dotenv.load_dotenv(env_file, override=True)
+        
+        self.logger = Logger("Neo4jPipeline").get_logger()
+        
+        self.model_name = os.environ["MODEL_LLM_NEO4J"]
 
         # Neo4j connection parameters
         self.neo4j_url = os.environ["NEO4J_URI"].replace("http", "bolt")
@@ -21,14 +27,30 @@ class Neo4jPipeline:
             username=self.neo4j_username,
             password=self.neo4j_password,
         )
+        
+        try:
+            self.graph.query("RETURN 1")
+            self.logger.info("Connessione a Neo4j stabilita con successo.")
+        except Exception as e:
+            self.logger.error(f"Errore durante la connessione a Neo4j: {e}")
+            raise ConnectionError(f"Errore durante la connessione a Neo4j: {e}")
 
         # Initialize embedding and LLM models
-        self.embedding_model = OllamaEmbeddings(model=model_name)
-        self.llm_model = ChatOllama(model=model_name)
+        self.embedding_model = OllamaEmbeddings(model=self.model_name)
+        self.llm_model = ChatOllama(model=self.model_name)
         self.index_name = index_name
 
     def load_data(self, csv_url):
-        """Load data into the Neo4j graph."""
+        """
+        Carica i dati da un file CSV nel grafo Neo4j.
+
+        Args:
+            csv_url (str): L'URL del file CSV contenente gli articoli da caricare.
+        
+        Raises:
+            Exception: Se c'è un errore durante il caricamento dei dati.
+        """
+
         q_load_articles = f"""
         LOAD CSV WITH HEADERS
         FROM '{csv_url}'
@@ -44,12 +66,26 @@ class Neo4jPipeline:
             MERGE (t:Topic {{name:trim(topic)}})
             MERGE (a)-[:IN_TOPIC]->(t))
         """
-        self.graph.query(q_load_articles)
+        
+        self.logger.info(f"Avvio caricamento dati da {csv_url}...")
+        try:
+            start_time = time.time()
+            self.graph.query(q_load_articles)
+            elapsed_time = time.time() - start_time
+            self.logger.info(f"Caricamento completato in {elapsed_time:.2f} secondi.")
+        except Exception as e:
+            self.logger.error(f"Errore durante il caricamento dati: {e}")
+        
         self.graph.refresh_schema()
 
-    def create_vector_index(self, node_label, text_node_properties, embedding_node_property):
-        """Create a vector index on the Neo4j graph."""
-        self.vector_index = Neo4jVector.from_existing_graph(
+    def query_similarity(self, query):
+        """Create a vector index on the Neo4j graph and perform a similarity-based query on the vector index."""
+        
+        node_label="Article"
+        text_node_properties=["topic", "title", "abstract"]
+        embedding_node_property="embedding"
+        
+        retriever = Neo4jVector.from_existing_graph(
             self.embedding_model,
             url=self.neo4j_url,
             username=self.neo4j_username,
@@ -57,17 +93,21 @@ class Neo4jPipeline:
             index_name=self.index_name,
             node_label=node_label,
             text_node_properties=text_node_properties,
-            embedding_node_property=embedding_node_property,
-        )
-
-    def query_similarity(self, query):
-        """Perform a similarity-based query on the vector index."""
-        retriever = self.vector_index.as_retriever()
+            embedding_node_property=embedding_node_property
+        ).as_retriever()
+        
         vector_qa = RetrievalQA.from_chain_type(
             llm=self.llm_model, chain_type="stuff", retriever=retriever
         )
-        result = vector_qa.invoke({"query": query})
-        return result["result"]
+        
+        self.logger.info(f"Esecuzione query di similarità: {query}")
+        try:
+            result = vector_qa.invoke({"query": query})
+            self.logger.info("Query di similarità completata.")
+            return result.get("result", "Nessun risultato trovato.")
+        except Exception as e:
+            self.logger.error(f"Errore durante la query di similarità: {e}")
+            return "Errore nell'elaborazione della query."
 
 # Usage example
 if __name__ == "__main__":
@@ -76,13 +116,6 @@ if __name__ == "__main__":
 
     pipeline.load_data(
         csv_url="https://raw.githubusercontent.com/dcarpintero/generative-ai-101/main/dataset/synthetic_articles.csv"
-    )
-
-    # Create vector index
-    pipeline.create_vector_index(
-        node_label="Article",
-        text_node_properties=["topic", "title", "abstract"],
-        embedding_node_property="embedding",
     )
 
     # Perform a similarity query
