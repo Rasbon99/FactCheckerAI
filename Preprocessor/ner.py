688e51f87d05b369dbcbf47b0a79cad07861368c
Leonardo Catello <leonardo.catello23@gmail.com>
2025-01-29 22:55:45 +0100

    comments update Preprocessor and logger explainability

--- Preprocessor/ner.py
+++ Preprocessor/ner.py
@@ -16,11 +16,19 @@ class NER:
         Initializes the NER class with a specific model and configures the Groq API client.
 
         Args:
-            env_file (str): The env file containing the API keys. Default: "key.env".
-            model (str): The model to use for Named Entity Recognition.
+            env_file (str): The env file containing the API keys. Default is "Pkey.env".
+            model (str, optional): The model to use for Named Entity Recognition. If not provided, the model is loaded from environment variables.
+        
         Attributes:
             model (str): The specified model for Groq.
             client (Groq): Instance of the Groq client for API requests.
+        
+        Returns:
+            None
+        
+        Raises:
+            FileNotFoundError: If the specified environment file is not found.
+            KeyError: If the model name is not found in the environment variables.
         """
         self.logger = Logger(self.__class__.__name__).get_logger()
         dotenv.load_dotenv(env_file, override=True)
@@ -29,6 +37,9 @@ class NER:
         
         if model:
             self.model = model
+        
+        if not self.model:
+            raise KeyError("Model name not found in the environment variables.")
 
     def extract_entities_and_topic(self, text, max_tokens=1024, temperature=0.5, stop=None):
         """
@@ -36,12 +47,17 @@ class NER:
 
         Args:
             text (str): The text from which to extract the topic and entities.
-            max_tokens (int): The maximum number of tokens for the completion. Default: 1024.
-            temperature (float): Controls randomness. Default: 0.5.
-            stop (str or None): Optional sequence indicating where the model should stop. Default: None.
-
+            max_tokens (int, optional): The maximum number of tokens for the completion. Default is 1024.
+            temperature (float, optional): Controls randomness. Default is 0.5.
+            stop (str or None, optional): Optional sequence indicating where the model should stop. Default is None.
+        
         Returns:
-            dict: A dictionary with extracted "topic" and "entities".
+            dict: A dictionary with the extracted "topic" and "entities".
+                Example: {"topic": "Technology", "entities": ["Elon Musk", "SpaceX", "Tesla", "Paris"]}.
+        
+        Raises:
+            Exception: If there is an error during the API request or entity extraction process.
+            json.JSONDecodeError: If the response cannot be parsed into a valid JSON format.
         """
         self.logger.info("Starting entity and topic extraction process.")
         self.logger.debug("Input text: %s...", text[:200]) 
