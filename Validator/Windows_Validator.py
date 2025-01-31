70e6a3331ada33ecb02aa252201f5433c5885c64
Rasbon99 <lollo.manco@gmail.com>
2025-01-31 19:37:50 +0100

    Update Windows_Validator.py
    Co-Authored-By: AuroraD-99 <173402035+AuroraD-99@users.noreply.github.com>
    Co-Authored-By: Leonardo Catello <leonardo.catello23@gmail.com>

--- Validator/Windows_Validator.py
+++ Validator/Windows_Validator.py
@@ -1,52 +1,38 @@
-from transformers import pipeline, AutoTokenizer, TFAutoModelForSequenceClassification, AutoConfig
-import os
-from dotenv import load_dotenv
-import tensorflow as tf
+from transformers import pipeline
 
-# Controllo delle GPU disponibili
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+class Validator:
+    def __init__(self, model_name: str):
+        """
+        Inizializza la classe Validator con il nome del modello.
+        
+        :param model_name: Il nome del modello pre-addestrato da utilizzare
+        """
+        self.model_name = model_name
+        # Creazione della pipeline zero-shot
+        self.zero_shot_classifier = pipeline("zero-shot-classification", model=self.model_name)
 
-gpus = tf.config.list_physical_devices('GPU')
-if gpus:
-    try:
-        tf.config.experimental.set_memory_growth(gpus[0], True)
-        print("GPU configurata con memory growth.")
-    except RuntimeError as e:
-        print(e)
+    def predict(self, texts, hypothesis_template: str):
+        """
+        Esegue la previsione sul testo dato utilizzando la classificazione zero-shot.
+        
+        :param texts: Una lista di testi da classificare
+        :param hypothesis_template: La frase da utilizzare come ipotesi per la classificazione
+        :return: I risultati della classificazione
+        """
+        return self.zero_shot_classifier(texts, candidate_labels=["entailment", "semi_entailment", "not_entailment"], hypothesis_template=hypothesis_template)
 
-# Carica le variabili dal file .env
-load_dotenv("key.env")
+# Esempio d'uso
+if __name__ == "__main__":
+    model_name = ""
+    validator = Validator(model_name)
 
-# Path del modello
-MODEL_PATH = os.getenv("MODEL_PATH_DEBERTA")
+    texts = [
+        "Defending Australian Open champion Jannik Sinner has returned to the quarterfinals, navigating an illness and multiple delays. Sinner beat No. 13 seed Holger Rune 6-3, 3-6, 6- 3, 6 -2. The world No. 1 put a “strange morning behind him” in returning to the Australian Open quarterfinals. The 22-year-old is a combined 11-0 against his potential next opponents De Minaur and Michelsen.",
+        "Jannik Sinner, what happened and how he is. The illness against Rune: the hands that shook and the difficulty in breathing. Very tough match for the world number 1 who beat the Dane and earned the pass to the quarterfinals of the Australian Open. Health problems between the second and third set: 'I felt dizzy, the conditions were difficult' di Sports Editorial 20 January 2025 Jannk Sinner assisted by Australian Open medical staff during match against Rune. Jannik begins to move badly on the court, It's slow and feels foggy . One point it seems to have stomach problems.",
+        "Jannik Sinner wins stop-start Australian Open match with Holger Rune after illness, net problems - The Athletic Tennis. World No. 1 Sinner won in four sets, 6-3. 3-6,6-3, 6 -2. Sinner appeared to indicate to his box that he was struggling with his movement on his left side. The defending Australian Open champion winced when running down a ball from the Danish No. 13 seed on his backhand side."
+    ]
 
-# Carica la configurazione del modello
-config = AutoConfig.from_pretrained(MODEL_PATH)
+    hypothesis = "The News: 'What's happening to Jannik Sinner? The Italian tennis player, number one in the world, felt ill during the match with Rune, the Dane, in the round of 16 of the Australian Open. He started to stagger in the third set, he lost his balance, he sat down during a change of court and was very pale, he was almost not breathing, he was about to vomit. Then he asked for the doctor's help, he went to the locker room for 12 minutes and came back, he was visibly better, he played a beautiful match.' is {}"
 
-# Verifica se le etichette sono presenti nella configurazione
-if hasattr(config, 'id2label'):
-    label_names = config.id2label
-    n_labels = config.num_labels
-    print("Numero di etichette:", n_labels)
-    print("Etichette del modello:", label_names)
-else:
-    print("Le etichette non sono disponibili nella configurazione.")
-    
-tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
-model = TFAutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
-
-# Premessa e ipotesi
-premise = "Emmanuel Macron is the President of USA"
-hypothesis = "Emmanuel Macron is the President of France"
-
-# Tokenizzazione
-inputs = tokenizer(premise, hypothesis, truncation=True, return_tensors="tf")
-
-# Inferenza
-outputs = model(inputs["input_ids"], attention_mask=inputs["attention_mask"])
-
-# Softmax e interpretazione
-predictions = tf.nn.softmax(outputs.logits, axis=-1).numpy()[0]
-label_names = ["entailment", "not_entailment"]
-prediction = {name: round(pred * 100, 1) for pred, name in zip(predictions, label_names)}
-print(prediction)
+    results = validator.predict(texts, hypothesis)
+    print(results)
