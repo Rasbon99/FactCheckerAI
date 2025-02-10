a65a1acd7ad7d42cd29ba204ea9cfb64c04155df
Leonardo Catello <leonardo.catello23@gmail.com>
2025-02-10 16:35:04 +0100

    Update ollama_entrypoint.sh

--- ollama_entrypoint.sh
+++ ollama_entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-# Start the Ollama service in the background
 /bin/ollama serve &
 SERVE_PID=$!
 sleep 5
-# Pull the desired model
 ollama pull phi3.5:latest
-# Wait for the ollama serve process
 wait $SERVE_PID
