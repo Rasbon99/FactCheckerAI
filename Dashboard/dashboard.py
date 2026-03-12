7099d73d16bcedef18ba8f73b7d9e5db283baaab
Alireza <alirezafazel379@gmail.com>
2026-03-12 15:08:42 +0100

    Formatted dashbord, and fixed warning

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -7,25 +7,26 @@ import dotenv
 import glob
 import streamlit as st
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from log import Logger
+
 
 class DashboardPipeline:
     def __init__(self, env_file="key.env"):
         """
-        Initializes the dashboard pipeline by loading environment variables 
+        Initializes the dashboard pipeline by loading environment variables
         and setting up the logger and session state.
-        
+
         Args:
             env_file (str): The path to the environment file containing necessary credentials and configurations.
-        
+
         Raises:
             Exception: If environment variables cannot be loaded or other initialization errors occur.
         """
         dotenv.load_dotenv(env_file, override=True)
         self.logger = Logger(self.__class__.__name__).get_logger()
-        self.logo = os.getenv('AI_IMAGE_UI')
-        self.controller_url = os.getenv('CONTROLLER_API_URL', 'http://127.0.0.1:8003')
+        self.logo = os.getenv("AI_IMAGE_UI", "Assets/FOX_AI.png")
+        self.controller_url = os.getenv("CONTROLLER_API_URL", "http://127.0.0.1:8003")
 
         # Load image into the sidebar
         self.image_sidebar = Image.open(self.logo)
@@ -35,24 +36,26 @@ class DashboardPipeline:
 
     def _initialize_session_state(self):
         """
-        Initializes session state variables for managing the chat messages 
+        Initializes session state variables for managing the chat messages
         and view mode (chat or history) in the Streamlit app.
-        
+
         Raises:
             Exception: If there is an error during session state initialization.
         """
         if "messages" not in st.session_state:
-            st.session_state["messages"] = [{"role": "assistant", "content": "To begin, state your claim..."}]
+            st.session_state["messages"] = [
+                {"role": "assistant", "content": "To begin, state your claim..."}
+            ]
         if "view_mode" not in st.session_state:
             st.session_state.view_mode = "chat"  # Default view mode
 
     def _log_error(self, msg):
         """
         Centralized error logging function to log errors and display them in Streamlit.
-        
+
         Args:
             msg (str): The error message to be logged and shown in the Streamlit app.
-        
+
         Raises:
             Exception: If there is an error during the logging process.
         """
@@ -62,7 +65,7 @@ class DashboardPipeline:
     def delete_chat_history(self):
         """
         Deletes the chat history by making a request to the controller server's endpoint.
-        
+
         Raises:
             requests.exceptions.RequestException: If there is an error in the POST request.
         """
@@ -76,10 +79,10 @@ class DashboardPipeline:
     def is_numeric_claim(self, claim_text):
         """
         Checks if the claim consists only of numeric characters.
-        
+
         Args:
             claim_text (str): The text of the claim to be checked.
-        
+
         Returns:
             bool: True if the claim is numeric, False otherwise.
         """
@@ -88,21 +91,20 @@ class DashboardPipeline:
     def get_response(self, claim):
         """
         Requests a response from the controller server for the given claim.
-        
+
         Args:
             claim (str): The claim text to be processed by the controller.
-        
+
         Raises:
             requests.exceptions.RequestException: If there is an error in the POST request.
-        
+
         Returns:
             dict: A dictionary containing the response, including title, summary, query result, sources, and images.
         """
         with st.spinner("Processing claim..."):
             try:
                 response = requests.post(
-                    f"{self.controller_url}/results",
-                    json={"text": claim}
+                    f"{self.controller_url}/results", json={"text": claim}
                 )
                 response.raise_for_status()
                 data = response.json()
@@ -111,10 +113,19 @@ class DashboardPipeline:
                 claim_title = result.get("claim_title", "")
                 claim_summary = result.get("claim_summary", "")
                 query_result = result.get("query_result", "")
-                sources = [{"title": src.get("title", ""), "url": src.get("url", "")} for src in result.get("sources", [])]
+                sources = [
+                    {"title": src.get("title", ""), "url": src.get("url", "")}
+                    for src in result.get("sources", [])
+                ]
 
                 images = self._load_images_from_folder(result.get("graphs_folder", ""))
-                return {'title': claim_title, 'summary': claim_summary, 'response': query_result, 'sources': sources, 'images': images}
+                return {
+                    "title": claim_title,
+                    "summary": claim_summary,
+                    "response": query_result,
+                    "sources": sources,
+                    "images": images,
+                }
             except requests.exceptions.RequestException as e:
                 self._log_error(f"Error in POST request: {e}")
                 return None
@@ -122,10 +133,10 @@ class DashboardPipeline:
     def _load_images_from_folder(self, folder):
         """
         Loads images from the specified folder and returns them as a list.
-        
+
         Args:
             folder (str): The path to the folder containing image files.
-        
+
         Returns:
             list: A list of PIL Image objects loaded from the folder.
         """
@@ -138,18 +149,20 @@ class DashboardPipeline:
                 except Exception as e:
                     self.logger.error(f"Error opening image {file}: {e}")
         else:
-            self.logger.warning(f"Graphs folder does not exist or was not specified: {folder}")
+            self.logger.warning(
+                f"Graphs folder does not exist or was not specified: {folder}"
+            )
         return images
 
     def display_message(self, role, message, avatar=None):
         """
         Displays a chat message in the Streamlit interface.
-        
+
         Args:
             role (str): The role of the sender, e.g., 'user' or 'assistant'.
             message (str): The content of the message to be displayed.
             avatar (str): The avatar to be used for the sender, default is 🦊.
-        
+
         Raises:
             Exception: If there is an error in displaying the message.
         """
@@ -162,10 +175,10 @@ class DashboardPipeline:
     def display_claim_response(self, response):
         """
         Displays the response from the system regarding the user's claim, including title, summary, sources, and images.
-        
+
         Args:
             response (dict): The response object containing claim title, summary, query result, sources, and images.
-        
+
         Raises:
             Exception: If there is an error during the display of the claim response.
         """
@@ -184,11 +197,11 @@ class DashboardPipeline:
 
         # Display sources if available
         with assistant_message.expander("📌 Sources"):
-            for src in response.get('sources', []):
+            for src in response.get("sources", []):
                 st.markdown(f"- [{src['title']}]({src['url']})")
 
         # Display images if available
-        images = response.get('images', [])
+        images = response.get("images", [])
         if images:
             with assistant_message.container():
                 cols = st.columns(len(images))
@@ -198,10 +211,10 @@ class DashboardPipeline:
     def get_conversations(self):
         """
         Retrieves all conversations from the controller server.
-        
+
         Raises:
             requests.exceptions.RequestException: If there is an error in the GET request.
-        
+
         Returns:
             list: A list of conversations.
         """
@@ -218,13 +231,14 @@ class DashboardPipeline:
     def display_conversation(self, conversation):
         """
         Displays a historical conversation in the chat interface.
-        
+
         Args:
             conversation (dict): The conversation data to be displayed, including title, claim, answer, sources, and images.
-        
+
         Raises:
             Exception: If there is an error in displaying the conversation.
         """
+
         def display_message_markdown(role, content, avatar=None):
             if role == "assistant":
                 message = st.chat_message("assistant", avatar="🦊")
@@ -236,8 +250,8 @@ class DashboardPipeline:
         content = f"""
         <h2 style='color: rgba(0, 0, 0, 0.9); font-size: 1.5em; line-height: 1.4;'>{conversation['title']}</h2>
         """
-        
-        if conversation['claim']:
+
+        if conversation["claim"]:
             content += f"<p style='color: rgba(0, 0, 0, 0.6); font-size: 1.1em; line-height: 1.6;'>{conversation['claim']}</p>"
 
         content += f"<p style='font-size: 1.1em; line-height: 1.6;'>{conversation['answer']}</p>"
@@ -247,35 +261,40 @@ class DashboardPipeline:
 
         # Display sources
         with st.expander("📌 Sources"):
-            if conversation['sources']:
-                sources_text = "\n\n".join([f"- [{src['title']}]({src['url']})" for src in conversation['sources']])
+            if conversation["sources"]:
+                sources_text = "\n\n".join(
+                    [
+                        f"- [{src['title']}]({src['url']})"
+                        for src in conversation["sources"]
+                    ]
+                )
                 st.markdown(sources_text)
             else:
                 st.markdown("No sources available for this claim.")
 
         # Display images
-        if conversation.get('images'):
-            cols = st.columns(len(conversation['images']))
-            for col, img in zip(cols, conversation['images']):
+        if conversation.get("images"):
+            cols = st.columns(len(conversation["images"]))
+            for col, img in zip(cols, conversation["images"]):
                 col.image(img)
 
     def get_conversation_by_id(self, convo_id):
         """
         Retrieves a specific conversation by its ID.
-        
+
         Args:
             convo_id (str): The ID of the conversation to be retrieved.
-        
+
         Returns:
             dict: The conversation with the specified ID, or None if not found.
         """
         conversations = self.get_conversations()
-        return next((convo for convo in conversations if convo['id'] == convo_id), None)
+        return next((convo for convo in conversations if convo["id"] == convo_id), None)
 
     def run(self):
         """
         Main function to run the Streamlit dashboard and manage the user interactions.
-        
+
         Raises:
             Exception: If there is an error during the execution of the dashboard.
         """
@@ -285,19 +304,24 @@ class DashboardPipeline:
             prompt = st.chat_input(max_chars=800, placeholder="Type your claim here...")
             if prompt:
                 if self.is_numeric_claim(prompt):
-                    self.display_message("assistant", "This claim is invalid because it consists only of numbers.")
+                    self.display_message(
+                        "assistant",
+                        "This claim is invalid because it consists only of numbers.",
+                    )
                     st.session_state.messages = []
                 else:
-                    st.session_state.messages.append({"role": "user", "content": prompt})
+                    st.session_state.messages.append(
+                        {"role": "user", "content": prompt}
+                    )
                     self.display_message("user", prompt)
 
                     if len(st.session_state.messages) > 1:
-                        claim = st.session_state.messages[-1]['content']
+                        claim = st.session_state.messages[-1]["content"]
                         response = self.get_response(claim)
                         self.display_claim_response(response)
 
         else:
-            if 'selected_conversation' in st.session_state:
+            if "selected_conversation" in st.session_state:
                 selected_conversation = st.session_state.selected_conversation
                 self.display_conversation(selected_conversation)
             else:
@@ -306,25 +330,30 @@ class DashboardPipeline:
         with st.sidebar:
 
             col1, col2, col3 = st.columns([0.8, 0.25, 0.2])
-            
-            with col1: 
+
+            with col1:
                 if st.button("➕ New Conversation", key="new_conv"):
                     st.session_state.view_mode = "chat"
                     st.session_state.messages = []
                     st.session_state.selected_conversation = None
 
                     # Hidden test message
-                    st.session_state.messages.append({"role": "user", "content": "Test automatic message for new conversation"})
+                    st.session_state.messages.append(
+                        {
+                            "role": "user",
+                            "content": "Test automatic message for new conversation",
+                        }
+                    )
                     st.rerun()
-            
-            with col2: 
+
+            with col2:
                 if st.button("🗑️", key="del_chat", help="Delete Chat History"):
                     # Supponiamo di avere definito una funzione per eliminare la cronologia
                     self.delete_chat_history()
                     st.session_state.view_mode = "chat"
                     st.session_state.messages = []
                     st.session_state.selected_conversation = None
-                    
+
                     st.rerun()
 
             with col3:
@@ -332,17 +361,28 @@ class DashboardPipeline:
                     st.stop()
 
             st.sidebar.title("Chat History")
-            st.sidebar.text_input("Search claims...", key="search_query", placeholder="Type to search")
+            st.sidebar.text_input(
+                "Search claims...", key="search_query", placeholder="Type to search"
+            )
 
             # Get conversations and filter them
             conversations = self.get_conversations()
-            filtered_conversations = [
-                convo for convo in conversations if st.session_state.search_query.lower() in convo.get("title", "").lower()
-            ] if "search_query" in st.session_state else conversations
-            filtered_conversations = filtered_conversations[::-1]  # Reverse order to show the latest first
+            filtered_conversations = (
+                [
+                    convo
+                    for convo in conversations
+                    if st.session_state.search_query.lower()
+                    in convo.get("title", "").lower()
+                ]
+                if "search_query" in st.session_state
+                else conversations
+            )
+            filtered_conversations = filtered_conversations[
+                ::-1
+            ]  # Reverse order to show the latest first
 
             # Initialize state for toggling view
-            if 'show_all_conversations' not in st.session_state:
+            if "show_all_conversations" not in st.session_state:
                 st.session_state.show_all_conversations = False
 
             # Limit the number of conversations to display
@@ -354,7 +394,10 @@ class DashboardPipeline:
 
             # Display the conversations
             for i, convo in enumerate(display_conversations):
-                if st.sidebar.button(convo['title'][:50] + ("..." if len(convo['title']) > 50 else ""), key=f"convo_{i}"):
+                if st.sidebar.button(
+                    convo["title"][:50] + ("..." if len(convo["title"]) > 50 else ""),
+                    key=f"convo_{i}",
+                ):
                     st.session_state.view_mode = "history"
                     st.session_state.selected_conversation = convo
                     st.rerun()
@@ -362,10 +405,15 @@ class DashboardPipeline:
             # Show toggle button for conversations
             if len(filtered_conversations) > max_display:
                 if st.sidebar.button(
-                    "Show Less Conversations" if st.session_state.show_all_conversations else "Show More Conversations"
+                    "Show Less Conversations"
+                    if st.session_state.show_all_conversations
+                    else "Show More Conversations"
                 ):
-                    st.session_state.show_all_conversations = not st.session_state.show_all_conversations
+                    st.session_state.show_all_conversations = (
+                        not st.session_state.show_all_conversations
+                    )
                     st.rerun()
+
 
 if __name__ == "__main__":
     dashboard = DashboardPipeline()
